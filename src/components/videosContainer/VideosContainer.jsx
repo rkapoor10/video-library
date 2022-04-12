7e59978f66426df25665d96313c919b21d35bb9d
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2022-04-12 07:22:42 +0530

    updated: home page with filters

--- src/components/videosContainer/VideosContainer.jsx
+++ src/components/videosContainer/VideosContainer.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./videosContainer.css";
 import VideoCard from "../cards/VideoCard";
+import { useVideo } from "../../context/VideoContext/VideoContext";
 
-const VideosContainer = ({ heading, videosData }) => {
+const VideosContainer = ({ heading }) => {
+  const { videosData } = useVideo();
+  console.log(videosData);
+  const filterVideos = (heading) => {
+    return videosData.filter((video) =>
+      video.tags.some((tag) => tag === heading)
+    );
+  };
+  const filteredVideos = filterVideos(heading);
+
   return (
     <div className="videos-container">
-      <div className="txt-m">{heading}</div>
+      <div className="txt-m">{heading} Videos</div>
       <div className="grid-column-layout">
-        {videosData.map((videoDetails) => (
+        {filteredVideos.map((videoDetails) => (
           <VideoCard key={videoDetails.id} details={videoDetails} />
         ))}
       </div>
